9d8585357e638898fc1b470525326ce4431065db
Pezhin <p@oath.pl>
2016-10-11 16:27:34 -0600

    Ceaseless issue 58

---
+++ tests/CeaselessDistill/app/src/main/java/com/peilunzhang/ceaselessdistill/MainActivity.java
@@ -0,0 +1,48 @@
+package com.peilunzhang.ceaselessdistill;
+
+import android.os.Bundle;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    private BuggyFragment bf;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, new BuggyFragment()).commit();
+
+    }
+
+    public class BuggyFragment extends Fragment{
+
+        TextView mText;
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+        }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_main, container);
+            mText = (TextView) v.findViewById(R.id.textMain);
+            mText.setText("success");
+            return v;
+        }
+    }
+}
+
