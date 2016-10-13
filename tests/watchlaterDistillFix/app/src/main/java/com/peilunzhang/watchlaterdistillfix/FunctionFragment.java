692203e0ff4aac8f2bca10242aa8c172e2ba9cee
Pezhin <p@oath.pl>
2016-10-13 09:32:05 -0600

    fix version of watchlater distill

---
+++ tests/watchlaterDistillFix/app/src/main/java/com/peilunzhang/watchlaterdistillfix/FunctionFragment.java
@@ -0,0 +1,35 @@
+package com.peilunzhang.watchlaterdistillfix;
+
+/**
+ * Created by Pezh on 10/11/16.
+ */
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+
+public class FunctionFragment extends Fragment {
+    private TextView textMain;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_function, container,
+                false);
+
+        return view;
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        textMain = (TextView) getView().findViewById(R.id.textMain);
+        textMain.setText("Fragment attached");
+
+    }
+
+}
+
+
