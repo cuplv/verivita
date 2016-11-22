732291b00a2c262c7e9f54cf866150052883aa47
Pezhin <p@oath.pl>
2016-11-21 23:19:16 -0700

    contractiontimer fix

---
+++ tests/distilled_apps/ContractionTimerDistilledFix/app/src/main/java/com/peilunzhang/contractiontimerdistilledfix/MainActivity.java
@@ -0,0 +1,39 @@
+package com.peilunzhang.contractiontimerdistilledfix;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    FragmentManager fragmentManager = getFragmentManager();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button firstFragmentBtn = (Button) findViewById(R.id.fragmentBtn1);
+        Button secondFragmentBtn = (Button) findViewById(R.id.fragmentBtn2);
+
+        firstFragmentBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                fragmentManager.beginTransaction().
+                        replace(R.id.fragment_container, new CountdownFragment()).commit();
+            };
+        });
+        secondFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                fragmentManager.beginTransaction().
+                        replace(R.id.fragment_container, new EmptyFragment()).commit();
+            };
+        });
+
+    }
+}
