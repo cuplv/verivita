aa0c59d79551f5e40755663dcbacf705cf7d62d2
Pezhin <p@oath.pl>
2016-11-23 02:39:32 -0700

    silence distill fix

---
+++ tests/distilled_apps/Silence_distill_fix/app/src/main/java/com/peilunzhang/silence_distill_fix/MainActivity.java
@@ -0,0 +1,42 @@
+package com.peilunzhang.silence_distill_fix;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_start;
+    Button btn_stop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_start = (Button) findViewById(R.id.fragmentBtn1);
+        btn_stop = (Button) findViewById(R.id.fragmentBtn2);
+
+        final EmptyFragment emp = new EmptyFragment();
+        final CountdownFragment cdt = new CountdownFragment();
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, cdt).commit();
+            };
+        });
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, emp).commit();
+            };
+        });
+
+    }
+
+
+
+
+
+}
