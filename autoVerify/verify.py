0400ee43b0027bf47361e0226c2c3292f131cc3b
Soon Lee Edmund Lam <edmund.lam@colorado.edu>
2017-01-18 12:07:44 -0700

    added verifier options in commandline exec

--- autoVerify/verify.py
+++ autoVerify/verify.py
@@ -7,6 +7,8 @@ from verifierChecks import runVerifierChecks, GOODTRACE, TRUNCTRACE, EXCEPTTRACE
 
 # -t /home/s/Documents/source/scratch/callback-verification/tests/realApplications/Kistenstapeln-Android/bug/trace_150117_0 -m bmc -s /home/s/Documents/source/callback-verification/cbverifier/android_specs/countdowntimer.spec:/home/s/Documents/source/callback-verification/cbverifier/android_specs/fragment.spec:/home/s/Documents/source/callback-verification/cbverifier/android_specs/button.spec:/home/s/Documents/source/callback-verification/cbverifier/android_specs/fragment_v4.spec:/home/s/Documents/source/callback-verification/cbverifier/android_specs/mediaplayer.spec:/home/s/Documents/source/callback-verification/cbverifier/android_specs/activity.spec:/home/s/Documents/source/callback-verification/cbverifier/android_specs/button_backup.spec -k 40 -z -i
 
+# python cbverifier/driver.py -t /data/cuplv/callback-verification-tests/callback-verification/tests/realApplications/Kistenstapeln-Android/bug/trace_150117_0 -m bmc -s /data/cuplv/callback-verification-specs/callback-verification/cbverifier/android_specs/countdowntimer.spec:/data/cuplv/callback-verification-specs/callback-verification/cbverifier/android_specs/fragment.spec:/data/cuplv/callback-verification-specs/callback-verification/cbverifier/android_specs/button.spec:/data/cuplv/callback-verification-specs/callback-verification/cbverifier/android_specs/fragment_v4.spec:/data/cuplv/callback-verification-specs/callback-verification/cbverifier/android_specs/mediaplayer.spec:/data/cuplv/callback-verification-specs/callback-verification/cbverifier/android_specs/activity.spec:/data/cuplv/callback-verification-specs/callback-verification/cbverifier/android_specs/button_backup.spec -k 40 -z -i
+
 # python verify.py /data/callback/curated/KistenstapelnDistill/good/TraceNoCrash.out proto /home/edmund/workshops/git_workshop/active/callback-verification-auto/callback-verification /home/edmund/workshops/git_workshop/active/callback-verification-specs/callback-verification/cbverifier/android_specs countdowntimer.spec,fragment.spec,button.spec,fragment_v4.spec,mediaplayer.spec,activity.spec,button_backup.spec
 
 # countdowntimer.spec:fragment.spec:button.spec:fragment_v4.spec:mediaplayer.spec:activity.spec:button_backup.spec
@@ -25,15 +27,16 @@ def runVerify(tracePath, specPaths, verifierPath, json=False, technique='bmc', s
 
 if __name__ == "__main__":
 
-   if len(sys.argv) != 6:
-      print "usage: python verify.py <Path to Trace File> <'proto' or 'json'> <Path to Verifier> <Path to Spec Repository> <Comma Separated List of Spec Files>"
+   if len(sys.argv) != 7:
+      print "usage: python verify.py <Path to Trace File> <'proto' or 'json'> <Path to Verifier> <Path to Spec Repository> <Comma Separated List of Spec Files> <Steps>"
       sys.exit(1)
 
    tracePath = sys.argv[1]
    traceType = sys.argv[2]
    verifierPath = sys.argv[3]
-   specPath     = sys.argv[4]
-   specFiles    = sys.argv[5]
+   specPath  = sys.argv[4]
+   specFiles = sys.argv[5]
+   steps     = sys.argv[6]
 
    if traceType == 'json':
       json = True
@@ -42,7 +45,7 @@ if __name__ == "__main__":
 
    specs = ":".join(map(lambda s: "%s/%s" % (specPath,s), specFiles.split(',')))
   
-   results = runVerify(tracePath, specs, verifierPath, json=json)
+   results = runVerify(tracePath, specs, verifierPath, json=json, steps=steps)
 
    print "Found bug: %s" % results['bugfound']
 
