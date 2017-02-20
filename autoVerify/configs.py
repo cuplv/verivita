a6f7cb330d66d79d4e2aa97303a6dd65fbdd3dc8
Soon Lee Edmund Lam <edmund.lam@colorado.edu>
2017-02-19 20:42:34 -0700

    added new config file and config fields for allow/disallow enable/disable specs

--- autoVerify/configs.py
+++ autoVerify/configs.py
@@ -2,6 +2,8 @@
 
 from ConfigParser import ConfigParser
 
+import os
+
 def get(conf, section, option, default=None):
      if conf.has_option(section, option):
          return conf.get(section, option).strip()
@@ -14,6 +16,27 @@ def splitClean(path, ls):
 def splitIt(ls):
     return map(lambda x: x.strip(), ls.split(','))
 
+def findAll(path, ext=None):
+   allfiles = []
+   for root, dirs, files in os.walk(path):
+       for file in files:
+           if ext == None or file.endswith("." + ext):
+                allfiles.append(os.path.join(root, file))
+   return allfiles
+
+def retrieveSpecParams(conf, section, specPathParam, specsParam, defaultSpecPath=None, defaultSpecs=None):
+   specPath = get(conf, section, specPathParam, default=defaultSpecPath)
+   specs    = get(conf, section, specsParam, default=defaultSpecs)
+   specPaths = []
+   if specPath != None and specs != None:
+      specFiles = splitIt(specs)
+      if 'ALL' not in specFiles:
+         specPaths = map( lambda x: "%s/%s" % (specPath,x), specFiles)
+      else:
+         specPaths = findAll(specPath, ext="spec")
+      # specPaths = ':'.join(specPaths)
+   return (specPath,specs,specPaths)
+
 # Extract configurations from config file.
 def getConfigs(iniFilePath='verifierConfig.ini'):
     conf = ConfigParser()
@@ -23,28 +46,42 @@ def getConfigs(iniFilePath='verifierConfig.ini'):
 
     verbose    = True if get(conf, vopts, 'verbose', default='False') == 'True' else False
     inputPath  = get(conf, vopts, 'input', default='/data/callback/output')
+    processedPath = get(conf, vopts, 'process', default='/data/callback/processed')
     checkedPath = get(conf, vopts, 'checked', default='/data/callback/checked')
     verifiedPath = get(conf, vopts, 'verified', default='/data/callback/verified')
     timeout = get(conf, vopts, 'timeout', default='300')
-
     verifierPath = get(conf, vopts, 'verifierpath', default='')
+
+    enSpecPath,enSpecs,enSpecPaths = retrieveSpecParams(conf, vopts, 'enablespecpath', 'enablespecs')
+    alSpecPath,alSpecs,alSpecPaths = retrieveSpecParams(conf, vopts, 'allowspecpath', 'allowspecs')
+
     specPath = get(conf, vopts, 'specpath', default='')
     specs = get(conf, vopts, 'specs', default='activity.spec,button.spec,countdowntimer.spec,fragment.spec,mediaplayer.spec')
  
     verifyGroups = get(conf, vopts, 'verifygroups', default='good,useless')
     verifySteps  = get(conf, vopts, 'verifysteps', default='40')
 
-    configs = { 'verbose':verbose, 'input':inputPath, 'checked':checkedPath, 'verified':verifiedPath
-              , 'verifier':verifierPath, 'specs':splitClean(specPath, specs), 'verifygroups':splitIt(verifyGroups)
-              , 'verifysteps':verifySteps, 'timeout':timeout }
+    configs = { 'verbose':verbose, 'input':inputPath, 'processed':processedPath 
+              , 'checked':checkedPath, 'verified':verifiedPath, 'verifier':verifierPath
+              , 'specs': ':'.join(enSpecPaths + alSpecPaths) # splitClean(specPath, specs)
+              , 'enspecs': ':'.join(enSpecPaths), 'alspecs': ':'.join(alSpecPaths)
+              , 'verifygroups':splitIt(verifyGroups), 'verifysteps':verifySteps, 'timeout':timeout }
 
     apps = {}
     for section in conf.sections():
        if section.startswith("app:"):
            appName = section[4:]
 
+           _,_,appEnSpecPaths = retrieveSpecParams(conf, section, 'enablespecpath', 'enablespecs'
+                                                  ,defaultSpecPath=enSpecPath
+                                                  ,defaultSpecs=enSpecs)
+           _,_,appAlSpecPaths = retrieveSpecParams(conf, section, 'allowspecpath', 'allowspecs'
+                                                  ,defaultSpecPath=alSpecPath
+                                                  ,defaultSpecs=alSpecs)
+
            appSpecPath = get(conf, section, 'specpath', default='')
            appSpecs = get(conf, section, 'specs', default=None)
+
            appJson  = True if get(conf, section, 'json', default='False') == 'True' else False
            appVerifyGroups = get(conf, section, 'verifygroups', default=None)
 
@@ -59,10 +96,13 @@ def getConfigs(iniFilePath='verifierConfig.ini'):
                appVerifyGroups = splitIt(appVerifyGroups)
 
            apps[appName] = { 'input'  : '%s/%s' % (inputPath,appName) 
+                           , 'processed' : '%s/%s' % (processedPath,appName)
                            , 'checked' : '%s/%s' % (checkedPath,appName)
                            , 'verified' : '%s/%s' % (verifiedPath,appName)
                            , 'json'   : appJson
-                           , 'specs'  : appSpecs
+                           , 'specs'  : ':'.join(appEnSpecPaths + appAlSpecPaths) # appSpecs
+                           , 'enspecs': ':'.join(appEnSpecPaths)
+                           , 'alspecs': ':'.join(appAlSpecPaths)
                            , 'verifygroups' : appVerifyGroups } 
     configs['apps'] = apps
 
