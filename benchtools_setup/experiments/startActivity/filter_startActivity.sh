db716bad66aecd62fc4a6560bd39eb289b80ce12
Shawn <shawn.meier@colorado.edu>
2017-04-14 14:27:53 -0600

    start activity filter

--- benchtools_setup/experiments/startActivity/filter_startActivity.sh
+++ benchtools_setup/experiments/startActivity/filter_startActivity.sh
@@ -1,3 +1,3 @@
 BASE_DIR=/home/s/Documents/data/monkey_traces/
 #python ~/Documents/source/TraceRunner/utils/ProtoConverter/filterProto.py --trace ${BASE_DIR}$1 --filter_class android.app.Fragment --filter_method "java.lang.String getString(int)"
-python ~/Documents/source/TraceRunner/utils/ProtoConverter/filterProto.py --trace ${BASE_DIR}$1 --filter_class android.support.v4.app.Fragment --filter_method "java.lang.String getString(int)"
+python ~/Documents/source/TraceRunner/utils/ProtoConverter/filterProto.py --trace ${BASE_DIR}$1 --filter_class android.app.Fragment --filter_method "void startActivity(android.content.Intent)"
