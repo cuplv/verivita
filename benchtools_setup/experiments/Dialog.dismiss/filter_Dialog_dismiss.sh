cc62c2465ffce3a2b5f68d721a4b30ac04b21efe
Ubuntu <ubuntu@ip-172-31-34-41.us-east-2.compute.internal>
2017-04-16 20:22:43 +0000

    trace filters

---
+++ benchtools_setup/experiments/Dialog.dismiss/filter_Dialog_dismiss.sh
@@ -0,0 +1,4 @@
+BASE_DIR=/home/s/Documents/data/monkey_traces/
+#python ~/Documents/source/TraceRunner/utils/ProtoConverter/filterProto.py --trace ${BASE_DIR}$1 --filter_class android.app.Fragment --filter_method "java.lang.String getString(int)"
+#void android.app.Dialog.dismiss()
+python ~/Documents/source/TraceRunner/utils/ProtoConverter/filterProto.py --trace ${BASE_DIR}$1 --filter_class android.app.Dialog --filter_method "void dismiss()"
