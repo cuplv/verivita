4fbc8ced6adff0ab460f1f747321f7a0f4482697
Shawn <shawn.meier@colorado.edu>
2017-04-16 22:33:48 -0600

    scrip to combine instances data

---
+++ benchtools_setup/instances/combine.sh
@@ -0,0 +1,10 @@
+rm Fragment_all.getString.txt Fragment_all.startActivity.txt Fragment_all.getResources.txt AsyncTask_all.execute.txt
+
+cat Fragmentv4.getString.txt >> Fragment_all.getString.txt
+cat Fragment.getString.txt >> Fragment_all.getString.txt
+cat Fragmentv4.startActivity.txt >> Fragment_all.startActivity.txt
+cat Fragment.startActivity.txt >> Fragment_all.startActivity.txt
+cat Fragment.getResources.txt >> Fragment_all.getResources.txt
+cat Fragmentv4.getResources.txt >> Fragment_all.getResources.txt
+cat AsyncTask.execute.txt >> AsyncTask_all.execute.txt
+cat AsyncTask.executeOnExecutor.txt >> AsyncTask_all.execute.txt
