63612bc2a438fe9c3c9bf3a16bff65afcc9421f7
Ubuntu <ubuntu@ip-172-31-29-145.us-east-2.compute.internal>
2017-06-14 22:00:22 +0000

    results 3 description

--- benchtools_setup/find_all_traces.sh
+++ benchtools_setup/find_all_traces.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm ./instances/*.txt 2>/dev/null
-find -L ~/Documents/data/monkey_traces/ -name "*repaired" > ./instances/allTraces.txt #overwrite old one
+find -L /home/ubuntu/Documents/data/fdroid_monkey -name "*repaired" > ./instances/allTraces.txt #overwrite old one
 #find ~/Documents/data/monkey_traces/ -name "trace-*" >> ./instances/allTraces.txt #append
 
 
