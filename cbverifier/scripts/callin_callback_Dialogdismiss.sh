18ecae3bbf16e4e39a86cef9043638c1f2240ff4
Shawn Meier <shawn.meier@colorado.edu>
2017-06-24 21:46:36 -0600

    generate config file

--- cbverifier/scripts/callin_callback_Dialogdismiss.sh
+++ cbverifier/scripts/callin_callback_Dialogdismiss.sh
@@ -3,4 +3,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-python $DIR/callin_callbacks.py --trace $1 --disallow /Users/s/Documents/source/callback-verification/cbverifier/android_specs/allowdisallow/android.app.AlertDialog/dismiss.spec:/Users/s/Documents/source/callback-verification/cbverifier/android_specs/subexpr/android.app.Activity/activity_callbacks.spec
+python $DIR/callin_callbacks.py --trace $1 --disallow /Users/s/Documents/source/callback-verification/cbverifier/android_specs/allowdisallow/android.app.AlertDialog/dismiss.spec:/Users/s/Documents/source/callback-verification/cbverifier/android_specs/subexpr/android.app.Activity/activity_callbacks.spec:/Users/s/Documents/source/callback-verification/cbverifier/android_specs/subexpr/android.app.AlertDialog/dialog.spec
