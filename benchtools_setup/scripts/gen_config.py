4eeab46be60d0a3b77d4aeb4016b8cfab808d4fa
Ubuntu <ubuntu@ip-172-31-29-145.us-east-2.compute.internal>
2017-06-25 04:12:55 +0000

    tarball instances typo

--- benchtools_setup/scripts/gen_config.py
+++ benchtools_setup/scripts/gen_config.py
@@ -103,5 +103,5 @@ if __name__ == "__main__":
                     if not os.path.isfile(specfile):
                         raise Exception("specfile: " + specfile + " does not exist")
                 print parameters_pref + ":".join(specfiles_absolute)
-                print tarball_pref + disallow + "_" + ruleset
-                print instance_pref + allow_disallow_rules[disallow][0] + ".tar.gz"
+                print tarball_pref + disallow + "_" + ruleset  + ".tar.gz"
+                print instance_pref + allow_disallow_rules[disallow][0]
