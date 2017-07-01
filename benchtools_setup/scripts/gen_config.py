d03a7355704c8c1e5f7db18b3ae7a37e6dffe855
Ubuntu <ubuntu@ip-172-31-29-145.us-east-2.compute.internal>
2017-07-01 15:01:41 +0000

    configuration generation increase time

--- benchtools_setup/scripts/gen_config.py
+++ benchtools_setup/scripts/gen_config.py
@@ -68,7 +68,7 @@ environment = "[environment]\n" \
 
 params = "executable =./scripts/verify.sh\n" \
     "basedir=/\n" \
-    "time_limit=1000\n" \
+    "time_limit=1500\n" \
     "mem_limit=4000000\n" \
     "email=\n"\
     "disabled=false"
