b5a888999de09ce0ee5457094722465f8666b7c6
Shawn Meier <shawn.meier@colorado.edu>
2018-08-10 13:03:21 -0600

    port change

--- web/example_queries/getCompletionSearch.sh
+++ web/example_queries/getCompletionSearch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -X GET http://localhost:9000/completion_search -d "@callin_hole.json" -H "Content-Type: application/JSON"
+curl -X GET http://localhost:8080/completion_search -d "@callin_hole.json" -H "Content-Type: application/JSON"
 
