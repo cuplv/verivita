2cab7cc3be7a618c95c2961698ca649cb09e2d9c
Shawn Meier <shawn.meier@colorado.edu>
2018-08-14 11:54:00 -0600

    adding and removing callins

--- web/verivita-web/app/assets/elm/TraceQuery.elm
+++ web/verivita-web/app/assets/elm/TraceQuery.elm
@@ -12,6 +12,7 @@ import Bootstrap.Card as Card
 import Html.Attributes exposing (class, src, style)
 import Bootstrap.Card.Block as Block
 import Bootstrap.Button as Button
+import Bootstrap.ListGroup as ListGroup
 
 
 main : Program Never Model Msg
@@ -67,6 +68,10 @@ type alias Model =
     {
         query : List QueryCallbackOrHole
     }
+emptyCallback =
+    QueryCallback( { frameworkClass = "", signature = "", receiver = Hole, commands = []})
+emptyCallin =
+    QueryCallin({ frameworkClass = "", signature = "", receiver = Hole})
 
 init : ( Model, Cmd Msg)
 init =
@@ -75,63 +80,160 @@ init =
 
 -- UPDATE
 
+iAddCallin old cipos =
+    case (old,cipos) of
+        (t, -1) -> QueryCommandHole :: t
+        (h :: t , 0) -> h :: QueryCommandHole :: t
+        (nil , 0) -> QueryCommandHole :: nil
+        (h :: t, a) -> h :: (iAddCallin t (a - 1))
+        (nil , a) -> nil -- TODO: error state
+
+iRemoveCallin old cipos =
+    case (old,cipos) of
+        (h :: t, 0) -> t
+        (h :: t, a) -> h :: (iRemoveCallin t (cipos - 1))
+        (nil,a) -> nil -- TODO: error state
+
+iFill old cipos =
+    case (old,cipos) of
+        (h :: t, 0) -> emptyCallin :: t
+        (h :: t, a) -> h :: iFill t (a - 1)
+        (nil,a) -> nil -- TODO: error state
+
+
+doCallin old cbpos cipos fn =
+    case (old,cbpos) of
+        (QueryCallback(d) :: t, 0) -> (QueryCallback({d | commands = fn d.commands cipos})) :: t
+        (h :: t, a) -> h :: (doCallin t (cbpos - 1) cipos fn)
+        (nil, a) -> nil --TODO: error state
+
 addHole old pos =
     case (old,pos) of
         (h :: old, 0) -> h :: QueryCallbackHole :: old
+        (nil, 0) -> QueryCallbackHole :: nil
         (h :: t, a) -> h :: addHole t (a-1)
-        (nil, a) -> nil
+        (nil, a) -> nil -- TODO: error state
+removeCOH old pos =
+    case (old,pos) of
+        (h :: old, 0) -> old
+        (h :: old, a) -> h :: (removeCOH old (a-1))
+        (nil,a) -> nil --TODO: error state
+
+
+fillHole old pos =
+    case (old,pos) of
+        (QueryCallbackHole :: old , 0) ->
+            emptyCallback :: old
+        (h :: old, a) -> h :: fillHole old (a-1)
+        (nil,a) -> nil --TODO: eror state
 
 type Msg
     = AddQueryCallbackAfter (Int)
     | AddQueryCallinAfter (Int, Int)
     | FillQueryCallbackHole (Int)
     | FillQueryCallinhole (Int,Int)
+    | RemoveQueryCallback (Int)
+    | RemoveQueryCallin (Int,Int)
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        AddQueryCallinAfter (callbackPos, callinPos) -> (model, Cmd.none) -- TODO
-        AddQueryCallbackAfter(callbackPos) -> ({model | query = addHole model.query callbackPos}, Cmd.none) -- TODO
-        FillQueryCallbackHole(callbackPos) -> (model, Cmd.none) -- TODO
-        FillQueryCallinhole(callbackPos, callinPos) -> (model, Cmd.none) --TODO
+        AddQueryCallinAfter (callbackPos, callinPos) ->
+            ({model | query = doCallin model.query callbackPos callinPos iAddCallin}, Cmd.none)
+        AddQueryCallbackAfter(callbackPos) -> ({model | query = addHole model.query callbackPos}, Cmd.none)
+        FillQueryCallbackHole(callbackPos) -> ({model | query = fillHole model.query callbackPos}, Cmd.none)
+        FillQueryCallinhole(callbackPos, callinPos) ->
+            ({model | query = doCallin model.query callbackPos callinPos iFill}, Cmd.none)
+        RemoveQueryCallback(callbackPos) -> ({model | query = removeCOH model.query callbackPos}, Cmd.none)
+        RemoveQueryCallin(callbackPos, callinPos) ->
+            ({model | query = doCallin model.query callbackPos callinPos iRemoveCallin}, Cmd.none)
 
 
 -- View
 
+sp =
+    Block.custom (text " ")
 holeCard pos =
     Card.block []
                 [ Block.titleH4 [] [ text "Hole" ]
                 , Block.text [] [ text (toString pos) ]
                 , Block.custom <|
                     Button.button [ Button.primary, Button.onClick (AddQueryCallbackAfter pos)] [ text "+" ] --
-                , Block.custom (text " ")
+                , sp
                 , Block.custom <|
-                    Button.button [ Button.primary ] [ text "-" ]
-                , Block.custom (text " ")
+                    Button.button [ Button.primary, Button.onClick (RemoveQueryCallback pos) ] [ text "-" ]
+                , sp
                 , Block.custom <|
-                    Button.button [ Button.secondary ] [ text "Fill" ]
+                    Button.button [ Button.secondary, Button.onClick (FillQueryCallbackHole pos) ] [ text "Fill" ]
+                , sp
+                , Block.custom <|
+                    Button.button [ Button.primary] [ text "Search"]
 
                 ]
 
-callbackCard c pos =
+callbackCard d pos =
     Card.block []
-                [ Block.titleH4 [] [ text "Card title" ]
-                , Block.text [] [ text "Some quick example text to build on the card title and make up the bulk of the card's content." ]
-                , Block.custom <|
-                    Button.button   [ Button.primary  ] [ text "+" ]
+                (
+                    [ Block.titleH4 [] [ text "Callback" ] ] ++
 
+                    (List.indexedMap (\i -> \a -> Block.custom (callinOrHoleCard pos i a) ) d.commands) ++
+
+                    [ Block.custom <|
+                        Button.button [ Button.primary, Button.onClick (AddQueryCallinAfter (pos, -1)) ] [ text "+ callin"]
+                    , sp
+                    , Block.custom <|
+                        Button.button [ Button.primary, Button.onClick (AddQueryCallbackAfter pos)  ] [ text "+ callback" ]
+                    , sp
+                    , Block.custom <|
+                        Button.button [ Button.primary, Button.onClick (RemoveQueryCallback pos) ] [text "- callback"]
+
+                    ]
+                )
+callinButtons cbpos cipos =
+    Card.block []
+        [
+            Block.custom <|
+                Button.button [ Button.primary, Button.onClick (RemoveQueryCallin (cbpos,cipos)) ] [ text "- callin"]
+            ,sp
+            ,Block.custom <|
+                Button.button [ Button.primary, Button.onClick (AddQueryCallinAfter (cbpos,cipos)) ] [ text "+ after"]
+            ,sp
+            ,Block.custom <|
+                Button.button [ Button.secondary, Button.onClick (FillQueryCallinhole (cbpos,cipos)) ] [ text "fill"]
+        ]
+callinCard callin cbpos cipos =
+        Card.config [ Card.attrs [ style [ ( "width", "25rem" ) ] ] ]
+            |> Card.header [ class "text-center" ]
+                [ h5 [ ] [ text "Callin" ]
                 ]
+            |> callinButtons cbpos cipos
+            |> Card.view
+
+callinHoleCard cbpos cipos =
+        Card.config [ Card.attrs [ style [ ( "width", "25rem" ) ] ] ]
+            |> Card.header [ class "text-center" ]
+                [ h5 [ ] [ text "Callin Hole" ]
+                ]
+
+            |> callinButtons cbpos cipos
+            |> Card.view
+
+
+callinOrHoleCard cbpos cipos c =
+    case c of
+        QueryCallin(d) -> callinCard d cbpos cipos
+        QueryCommandHole -> callinHoleCard cbpos cipos
 
 callbackOhrHoleCard contents =
-    Card.config [ Card.attrs [ style [ ( "width", "20rem" ) ] ] ]
+    Card.config [ Card.attrs [ style [ ( "width", "30rem" ) ] ] ]
         |> Card.header [ class "text-center" ]
-            [ h3 [ ] [ text "" ]
+            [ h3 [ ] [ text "Callback" ]
             ]
         |> contents
         |> Card.view
 
 drawCallbackOrHole pos c =
     case c of
-        QueryCallback(d) -> callbackOhrHoleCard (callbackCard c pos)
+        QueryCallback(d) -> callbackOhrHoleCard (callbackCard d pos)
         QueryCallbackHole -> callbackOhrHoleCard (holeCard pos)
 
 
