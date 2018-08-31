df1ff37c1a5667609e8295663cb3fbc03f5c02ad
Shawn Meier <shawn.meier@colorado.edu>
2018-08-31 00:49:34 -0600

    docker stuff for cbverifier web

---
+++ web/cbverifier-web/docker/build_docker_container.sh
@@ -0,0 +1,11 @@
+mkdir verivita
+mkdir verivita/software
+mkdir verivita/app
+cp -r ~/Documents/source/verivita/cbverifier verivita/cbverifier
+tar -xf ~/Documents/source/verivita/nuXmv-1.1.1-linux64.tar.gz --directory verivita/software/
+cp ~/Documents/source/verivita/web/cbverifier-web/*.py verivita/app/
+
+docker build -t fixr_cbverifier .
+
+rm -r verivita
+
