2276be17abdda1e35c80b149abce8ad606079d90
Shawn Meier <shawn.meier@colorado.edu>
2018-09-01 15:59:09 -0600

    docker deployment seems to work

--- web/trace-query/dockercontainer/buildContainer.sh
+++ web/trace-query/dockercontainer/buildContainer.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cp ../target/universal/trace-query-1.0-SNAPSHOT.zip .
 unzip trace-query-1.0-SNAPSHOT.zip
 docker build -t fixr_verivita_web_search .
--- web/verivita-web/docker_container/buildContainer.sh
+++ web/verivita-web/docker_container/buildContainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp ../target/universal/vvweb-1.0-SNAPSHOT.zip .
 unzip vvweb-1.0-SNAPSHOT.zip
-docker build -t vvweb .
+docker build -t fixr_verivita_vvweb .
 rm -r vvweb-1.0-SNAPSHOT*
 
